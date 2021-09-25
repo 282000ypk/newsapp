19b588d6faf9c8e3762cb35756f6733c4a13d771
yogesh <ypk282000@gmail.com>
2021-09-25 19:38:56 +0530

    some css changes and a python retrive changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from newsapi import NewsApiClient
 from textblob import TextBlob
 from flask import jsonify
 import json
+import os
 
 app =  Flask(__name__)
  
@@ -12,7 +13,7 @@ with open("allnews.json","w") as all:
 	newsapi = NewsApiClient(api_key='63d39c686718447fb0d1ccc422c98029')
 
 	# # /v2/top-headlines
-	top_headlines = newsapi.get_top_headlines(q='',language='en',country='in')
+	top_headlines = newsapi.get_everything(q='',language='en',country='in')
 
 	# # /v2/everything
 	# print(top_headlines["totalResults"])
@@ -36,31 +37,14 @@ def main():
 	return render_template("index.html")
 
 @app.route("/allnews")
-def readnews():
-	
-
-	# # Init
-	newsapi = NewsApiClient(api_key='63d39c686718447fb0d1ccc422c98029')
-
-	# # /v2/top-headlines
-	top_headlines = newsapi.get_top_headlines(q='',language='en',country='in')
-
-	# # /v2/everything
-	# print(top_headlines["totalResults"])
-
-	i=0;
-	for article in top_headlines["articles"]:
-	    testimonial = TextBlob(article["title"])
-	    if(testimonial.sentiment.polarity<0):
-	        print("negative")
-	        top_headlines["articles"][i]["sentiment"]="negative"
-	    else:
-	        print("posiitive")
-	        top_headlines["articles"][i]["sentiment"]="positive"
-	    i+=1
-	# print(top_headlines["articles"][0])
-
-	return render_template("readnews.html",data=top_headlines)
+def allnews():
+	last_modified = os.path.getmtime("allnews.json")
+	print(last_modified)
+	allnews_json=None
+	with open("allnews.json","r") as allnews:
+		allnews_json = json.load(allnews)
+		print(allnews_json["articles"][0])
+	return render_template("readnews.html",data=allnews_json)
 
 if __name__ == '__main__':
 	app.run(debug=True)
